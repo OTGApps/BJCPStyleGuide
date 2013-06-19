938f9d000c607b0dfb1a300b8ba830b5df4cd82b
Mark Rickert <mjar81@gmail.com>
2013-06-18 20:15:22 -0400

    Detail screen almost finished.

--- app/Screens/DetailScreen.rb
+++ app/Screens/DetailScreen.rb
@@ -1,50 +1,142 @@
-class DetailScreen < PM::Screen
+class DetailScreen < PM::WebScreen
 
-  attr_accessor :name, :path
-
-  def on_init
-    self.setTitle self.name
-  end
+  attr_accessor :data, :cell
 
   def will_appear
     @view_loaded ||= begin
-      self.view = UIWebView.new
-      self.view.delegate = self
+      self.setTitle self.cell[:title]
 
-      content_string = File.read(self.path)
-      baseURL = NSURL.fileURLWithPath(App.resources_path)
+      self.navigationController.setToolbarHidden(false)
 
-      #Convert images over to retina if the images exist.
-      if Device.retina?
-        content_string.gsub!(/src=['"](.*?)\.(jpg|gif|png)['"]/) do |img|
-          if File.exists?(App.resources_path + "/#{$1}@2x.#{$2}")
-            # Create the image only so that we can get its actual size
-            uiImage = UIImage.imageNamed("/#{$1}@2x.#{$2}")
+      flexible_space = UIBarButtonItem.alloc.initWithBarButtonSystemItem(
+        UIBarButtonSystemItemFlexibleSpace,
+        target:nil,
+        action:nil)
 
-            newWidth = uiImage.size.width / 2
-            newHeight = uiImage.size.height / 2
+      increase_size = UIBarButtonItem.alloc.initWithTitle(
+        "A",
+        style: UIBarButtonItemStyleBordered,
+        target: self,
+        action: :increase_size)
 
-            img = "src=\"#{$1}@2x.#{$2}\" width=\"#{newWidth}\" height=\"#{newHeight}\""
-          end
-        end
+      decrease_size = UIBarButtonItem.alloc.initWithTitle(
+        "a",
+        style: UIBarButtonItemStyleBordered,
+        target: self,
+        action: :decrease_size)
+
+      self.toolbarItems = [flexible_space, increase_size, decrease_size]
+
+      if App::Persistence['font_size'].nil?
+        App::Persistence['font_size'] = 100
       end
 
-      # Prepend the stylesheet to the html document before displaying.
-      content_string = '<link href="style.css" rel="stylesheet" type="text/css" />' + content_string
-      # Prepend the name of the file.
-      content_string = "<h1>#{self.name}</h1>" + content_string
-
-      self.view.loadHTMLString(content_string, baseURL:baseURL)
     end
   end
 
-  #Open UIWebView delegate links in Safari.
-  def webView(inWeb, shouldStartLoadWithRequest:inRequest, navigationType:inType)
-    if inType == UIWebViewNavigationTypeLinkClicked
-      UIApplication.sharedApplication.openURL(inRequest.URL)
-      return false #don't allow the web view to load the link.
+  def load_finished
+    change_size
+    set_srm_range
+  end
+
+  def content
+    <<-CONTENT
+
+#{css}
+
+<div class="srmrange">&nbsp;</div>
+<h1>#{self.cell[:title]}</h1>
+<div class="srmrange">&nbsp;</div>
+
+<h2>Aroma</h2>
+<p>#{aroma}</p>
+
+<h2>Appearance</h2>
+<p>#{appearance}</p>
+
+<h2>Flavor</h2>
+<p>#{flavor}</p>
+
+<h2>Mouthfeel</h2>
+<p>#{mouthfeel}</p>
+
+<h2>Overall Impression</h2>
+<p>#{impression}</p>
+
+<h2>Comments</h2>
+<p>#{comments}</p>
+
+<h2>History</h2>
+<p>#{history}</p>
+
+<h2>Ingredients</h2>
+<p>#{ingredients}</p>
+
+<h2>Vital Statistics</h2>
+<ul>#{vital_stats}</ul>
+
+<h2>Commercial Examples</h2>
+<p>#{examples}</p>
+
+    CONTENT
+  end
+
+  def css
+    "<style>" << File.read(File.join(App.resources_path, "style.css")) << "</style>"
+  end
+
+  def vital_stats
+    table = ""
+
+    %w(og fg ibu srm abv).each do |stat|
+      unless stats[stat].nil?
+        table << "<li>" + stat.upcase + ": " + vital_stats_low_high(stats[stat]) + "</li>"
+      end
     end
-    true #return true for local file loading.
+    table
+  end
+
+  def vital_stats_low_high(stat)
+    if stat["flexible"] == "false"
+      stat["low"] + "-" + stat["high"]
+    else
+      "N/A"
+    end
+  end
+
+  def set_srm_range
+    if defined? stats["srm"]["low"]
+      gradient = SRM.css_gradient(stats["srm"]["low"], stats["srm"]["high"])
+    else
+      gradient = "display:none;"
+    end
+    evaluate "document.body.innerHTML += '<style>.srmrange{#{gradient}}</style>'"
+  end
+
+  def increase_size(args={})
+    App::Persistence['font_size'] = App::Persistence['font_size'] + 10
+    change_size
+  end
+
+  def decrease_size(args={})
+    App::Persistence['font_size'] = App::Persistence['font_size'] - 10
+    change_size
+  end
+
+  def change_size
+    evaluate "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '#{App::Persistence['font_size']}%'",
+  end
+
+  def method_missing(meth, *args, &block)
+    unless self.data[meth.to_s].nil?
+      if self.data[meth.to_s].is_a? String
+        self.data[meth.to_s].gsub("[em]", "<em>").gsub("[/em]", "</em>")
+      else
+        self.data[meth.to_s]
+      end
+    else
+      "N/A"
+    end
   end
 
 end
