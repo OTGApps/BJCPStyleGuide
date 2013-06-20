3e58c49b2c16ee481008ebc18d4766181e881198
Mark Rickert <mjar81@gmail.com>
2013-06-19 22:20:29 -0400

    Add a "made in" label to the about view.

--- app/Screens/AboutScreen.rb
+++ app/Screens/AboutScreen.rb
@@ -9,7 +9,26 @@ class AboutScreen < PM::WebScreen
   def will_appear
     @view_loaded ||= begin
       set_nav_bar_right_button "Done", action: :close_modal, type: UIBarButtonItemStyleDone
+
+      self.navigationController.setToolbarHidden(false)
+      self.toolbarItems = [flexible_space, made_in_label, flexible_space]
+
     end
+  end
+
+  def made_in_label
+    label = set_attributes UILabel.alloc.initWithFrame(CGRectZero), {
+      frame: CGRectMake(0.0 , 11.0, view.frame.size.width, 21.0),
+      font: UIFont.fontWithName("Helvetica", size:16),
+      background_color: UIColor.clearColor,
+      text: "Made in Beautiful Charlotte, NC",
+      text_alignment: UITextAlignmentCenter
+    }
+    UIBarButtonItem.alloc.initWithCustomView(label)
+  end
+
+  def flexible_space
+    UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemFlexibleSpace, target:nil, action:nil)
   end
 
   def close_modal
