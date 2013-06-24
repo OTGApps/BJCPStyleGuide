9e65be7065c58a38624bbbef704b9df5d1321e5e
Mark Rickert <mjar81@gmail.com>
2013-06-24 16:20:59 -0400

    Extract resizing webviews into their own class that is inherited by others.

--- app/Screens/DetailScreen.rb
+++ app/Screens/DetailScreen.rb
@@ -1,4 +1,4 @@
-class DetailScreen < PM::WebScreen
+class DetailScreen < SizeableWebScreen
   attr_accessor :style, :cell
 
   def will_appear
@@ -20,37 +20,14 @@ class DetailScreen < PM::WebScreen
 
   def on_appear
     return if self.cell.nil?
+    super
 
-    flexible_space = UIBarButtonItem.alloc.initWithBarButtonSystemItem(
-      UIBarButtonSystemItemFlexibleSpace,
-      target:nil,
-      action:nil)
-
-    increase_size = UIBarButtonItem.alloc.initWithTitle(
-      "A",
-      style: UIBarButtonItemStyleBordered,
-      target: self,
-      action: :increase_size)
-
-    decrease_size = UIBarButtonItem.alloc.initWithTitle(
-      "a",
-      style: UIBarButtonItemStyleBordered,
-      target: self,
-      action: :decrease_size)
-
-
-    toolbar_animated = Device.ipad? ? false : true
-    self.navigationController.setToolbarHidden(false, animated:toolbar_animated)
-    self.toolbarItems = [flexible_space, decrease_size, increase_size]
-
-    unless self.cell.nil?
-      flurry_params = {style: self.cell[:title]}
-      Flurry.logEvent("ViewedStyle", withParameters:flurry_params) unless Device.simulator?
-    end
+    flurry_params = {style: self.cell[:title]}
+    Flurry.logEvent("ViewedStyle", withParameters:flurry_params) unless Device.simulator?
   end
 
   def load_finished
-    change_size
+    super
     set_srm_range
   end
 
@@ -90,29 +67,6 @@ class DetailScreen < PM::WebScreen
       gradient = SRM.css_gradient(style.srm_range) unless style.nil?
     end
     evaluate "document.body.innerHTML += '<style>.srmrange{#{gradient}}</style>'"
-  end
-
-  def increase_size(args={})
-    size_mover(10)
-  end
-
-  def decrease_size(args={})
-    size_mover(-10)
-  end
-
-  def size_mover(which_way)
-    App::Persistence['font_size'] = App::Persistence['font_size'] + which_way
-    change_size
-  end
-
-  def change_size
-    evaluate "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '#{App::Persistence['font_size']}%'",
-  end
-
-  def open_info_screen(args={})
-    open_modal AboutScreen.new(external_links: true),
-      nav_bar: true,
-      presentation_style: UIModalPresentationFormSheet,
   end
 
 end
