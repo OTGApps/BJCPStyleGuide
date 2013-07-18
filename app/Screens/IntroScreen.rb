44b0a336ebd8dc01c4b823c9ece702bb7e9fbf14
Mark Rickert <mjar81@gmail.com>
2013-07-18 01:43:13 -0400

    will_appear -> on_load

--- app/Screens/IntroScreen.rb
+++ app/Screens/IntroScreen.rb
@@ -2,17 +2,18 @@ class IntroScreen < SizeableWebScreen
 
   attr_accessor :file
 
+  def on_load
+    if Device.ipad?
+      set_nav_bar_right_button UIImage.imageNamed("info.png"), action: :open_about_screen
+    end
+  end
+
   def content
+    ap self.file
     self.file
   end
 
   def will_appear
-    @view_loaded ||= begin
-      if Device.ipad?
-        set_nav_bar_right_button UIImage.imageNamed("info.png"), action: :open_about_screen
-      end
-    end
-
     flurry_params = {category: self.title}
     Flurry.logEvent("IntroViewed", withParameters:flurry_params) unless Device.simulator?
   end
@@ -21,6 +22,5 @@ class IntroScreen < SizeableWebScreen
     super
     css = "#srmtable {float:right;width:50%;}#srmimage{width:50%;float:left;}.clear {clear:both;}"
     evaluate "document.body.innerHTML += '<style>#{css}</style>'" if Device.ipad?
-
   end
 end
