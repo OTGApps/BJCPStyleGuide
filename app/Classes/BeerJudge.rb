31735958aa97970cbd338590e9a5eacca70431b4
Mark Rickert <mjar81@gmail.com>
2015-08-15 13:00:48 -0600

    update BeerJudge class

--- app/Classes/BeerJudge.rb
+++ app/Classes/BeerJudge.rb
@@ -1,17 +1,19 @@
 class BeerJudge
 
-  def self.is_installed?
-    beer_judge_url_scheme = NSURL.URLWithString BeerJudge.url_scheme
-    UIApplication.sharedApplication.canOpenURL(beer_judge_url_scheme)
-  end
+  class << self
+    def is_installed?
+      beer_judge_url_scheme = NSURL.URLWithString BeerJudge.url_scheme
+      UIApplication.sharedApplication.canOpenURL(beer_judge_url_scheme)
+    end
 
-  def self.open(url)
-    url = NSURL.URLWithString "#{BeerJudge.url_scheme}//#{url}"
-    UIApplication.sharedApplication.openURL url
-  end
+    def open(url)
+      url = NSURL.URLWithString "#{BeerJudge.url_scheme}//#{url}"
+      UIApplication.sharedApplication.openURL url
+    end
 
-  def self.url_scheme
-    "beerjudge:"
+    def url_scheme
+      "beerjudge:"
+    end
   end
 
 end
