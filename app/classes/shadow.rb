7d51489a2fa0cddfe10f7b6dcd40fefebcff01a3
Mark Rickert <mjar81@gmail.com>
2015-08-16 12:17:46 -0600

    Remove shadow class

--- app/classes/shadow.rb
+++
@@ -1,9 +0,0 @@
-# Author: Michael Forrest | Good To Hear | http://goodtohear.co.uk | License terms: credit me.
-class Shadow
-  def self.addTo view
-    view.layer.shadowColor = UIColor.blackColor.CGColor
-    view.layer.shadowOpacity = 0.3
-    view.layer.shadowRadius = 4
-    view.layer.shadowOffset = [0,2]
-  end
-end
